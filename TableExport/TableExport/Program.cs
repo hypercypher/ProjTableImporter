9b67d284897d56200992e79c68933c05721d02ba
hycy <chrisphamlee@gmail.com>
2013-08-18 20:37:06 -0500

    AW Change

--- TableExport/TableExport/Program.cs
+++ TableExport/TableExport/Program.cs
@@ -19,7 +19,7 @@ namespace TableImporter
     {
         static void Main(string[] args)
         {
-            // TESTING Commit into GITHUB 3333333
+            // TESTING Commit into GITHUB 444444
             Console.WriteLine("Enter file name: ");
             string filename = Console.ReadLine().ToString();
             FileInfo fi = new FileInfo(string.Format("C:\\users\\hycy_tabby\\Databases\\{0}.txt",filename));
